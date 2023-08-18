d6dc0cfc649c161cd321453145344673aad5ba5d
MesakDuduCoder <duduaoa99@gmail.com>
2023-08-18 22:07:48 +0530

    Query mutiple tables

--- data.sql
+++ data.sql
@@ -67,3 +67,119 @@ VALUES (
     true,
     11
     );
+
+INSERT INTO owners ( 
+    full_name, 
+    age
+    ) 
+VALUES (
+    'Sam Smith',
+    34
+    );
+
+INSERT INTO owners ( 
+    full_name, 
+    age
+    ) 
+VALUES (
+    'Jennifer Orwell',
+    19
+    );
+
+INSERT INTO owners ( 
+    full_name, 
+    age
+    ) 
+VALUES (
+    'Bob',
+    45
+    );
+
+INSERT INTO owners ( 
+    full_name, 
+    age
+    ) 
+VALUES (
+    'Melody Pond',
+    77
+    );
+
+INSERT INTO owners ( 
+    full_name, 
+    age
+    ) 
+VALUES (
+    'Dean Winchester',
+    14
+    );
+
+INSERT INTO owners ( 
+    full_name, 
+    age
+    ) 
+VALUES (
+    'Jodie Whittaker',
+    38
+    );
+
+INSERT INTO species ( 
+    name
+    ) 
+VALUES (
+    'Pokemon'
+    );
+
+INSERT INTO species ( 
+    name
+    ) 
+VALUES (
+    'Digimon'
+    );
+
+UPDATE animals
+SET species_id = 2
+WHERE animals.name LIKE '%mon%';
+
+UPDATE animals
+SET species_id = 1
+WHERE species_id IS NULL;
+
+UPDATE animals
+SET owner_id = 1
+WHERE name = 'Agumon';
+
+UPDATE animals
+SET owner_id = 2
+WHERE name = 'Gabumon';
+
+UPDATE animals
+SET owner_id = 2
+WHERE name = 'Pikachu';
+
+UPDATE animals
+SET owner_id = 3
+WHERE name = 'Devimon';
+
+UPDATE animals
+SET owner_id = 3
+WHERE name = 'Plantmon';
+
+UPDATE animals
+SET owner_id = 4
+WHERE name = 'Charmander';
+
+UPDATE animals
+SET owner_id = 4
+WHERE name = 'Squirtle';
+
+UPDATE animals
+SET owner_id = 4
+WHERE name = 'Blossom';
+
+UPDATE animals
+SET owner_id = 5
+WHERE name = 'Angemon';
+
+UPDATE animals
+SET owner_id = 5
+WHERE name = 'Boarmon';
